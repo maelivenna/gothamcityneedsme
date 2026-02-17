755bc5bb7bebd46654f9de5b4be47be07f7922c4
maelivenna <m_maratova@kbtu.kz>
2026-02-17 13:19:56 +0500

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,47 @@
+//i'm javascript text
+alert("I'm JavaScript!");
+
+
+//external script
+alert("I'm JavaScript!");
+
+
+//working with variables
+let admin, name1;
+
+name1 = "John";
+
+admin = name1;
+
+alert(admin);
+
+
+
+//giving right name
+let ourPlanetName = "Earth";
+
+let currentUserName = "John";
+
+
+
+//uppercase const
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
+
+
+
+//string quotes
+let name2 = "Ilya";
+
+alert( `hello ${1}` ); // "hello 1"
+
+alert( `hello ${"name"}` ); // "hello name"
+
+alert( `hello ${name2}` ); // "hello namee"
+
+
+
+//a simple page
+let name3 = prompt("What's ur name?");
+alert(name3);
